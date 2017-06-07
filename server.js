1aef7440a51e838eb2552bba4b7af2a570b12d6b
mwong770 <mwong770@gmail.com>
2017-06-07 16:49:07 -0400

    fixes quotes not meeting standard

--- server.js
+++ server.js
@@ -2,31 +2,31 @@
 var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
-var exphbs = require('express-handlebars');
-var swapsRoutes = require('./controllers/swaps_controller.js');
+var exphbs = require("express-handlebars");
+var swapsRoutes = require("./controllers/swaps_controller.js");
 var db = require("./models");
 
 //*** auth part 1 starts; will combine with other code and remove ** when auth work done *****
 
-var path = require('path');
-var favicon = require('serve-favicon');
-var logger = require('morgan');
-var cookieParser = require('cookie-parser');
-var session = require('express-session');
-var dotenv = require('dotenv');
-var passport = require('passport');
-var Auth0Strategy = require('passport-auth0');
+var path = require("path");
+var favicon = require("serve-favicon");
+var logger = require("morgan");
+var cookieParser = require("cookie-parser");
+var session = require("express-session");
+var dotenv = require("dotenv");
+var passport = require("passport");
+var auth0Strategy = require("passport-auth0");
 
 dotenv.load();
 
-var authRoutes = require('./controllers/auth_controller.js');
+var authRoutes = require("./controllers/auth_controller.js");
 
 // This will configure Passport to use Auth0
 var strategy = new Auth0Strategy({
         domain:       process.env.AUTH0_DOMAIN,
         clientID:     process.env.AUTH0_CLIENT_ID,
         clientSecret: process.env.AUTH0_CLIENT_SECRET,
-        callbackURL:  process.env.AUTH0_CALLBACK_URL || 'http://localhost:3000/callback'
+        callbackURL:  process.env.AUTH0_CALLBACK_URL || "http://localhost:3000/callback"
     }, function(accessToken, refreshToken, extraParams, profile, done) {
         return done(null, profile);
 });
@@ -54,11 +54,11 @@ app.use(methodOverride("_method"));
 
 //*** auth part 2 starts; will combine with other code and remove ** when auth work done *****
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(bodyParser.json());
 app.use(cookieParser());
 app.use(session({
-    secret: 'shhhhhhhhh',
+    secret: "shhhhhhhhh",
     resave: true,
     saveUninitialized: true
 }));
