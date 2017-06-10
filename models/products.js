3df9d59be7498f608b9e75ae03003327c705a462
mwong770 <mwong770@gmail.com>
2017-06-10 00:40:37 -0400

    adds user info form and confirmation

--- models/products.js
+++ models/products.js
@@ -1,34 +1,34 @@
-module.exports = function(sequelize, DataTypes) {
-    var Products = sequelize.define("Products", { // 2nd argument in define method for columns 
-            product_name: {
-            	type: DataTypes.STRING,
-            	allowNull: false,
-            	validate: { len:[1] }
-            },// 1st col
+// module.exports = function(sequelize, DataTypes) {
+//     var Products = sequelize.define("Products", { // 2nd argument in define method for columns 
+//             product_name: {
+//             	type: DataTypes.STRING,
+//             	allowNull: false,
+//             	validate: { len:[1] }
+//             },// 1st col
 
-            product_description: {
-            	type: DataTypes.STRING,
-            	allowNull: false,
-            	validate: { len:[1] }
-            }, //2nd col
+//             product_description: {
+//             	type: DataTypes.STRING,
+//             	allowNull: false,
+//             	validate: { len:[1] }
+//             }, //2nd col
 
-            product_img: {
-            	type: DataTypes.TEXT,
-            	allowNull: true,
-            } //3rd col
-        },// end of the 2nd argument in define method
+//             product_img: {
+//             	type: DataTypes.TEXT,
+//             	allowNull: true,
+//             } //3rd col
+//         },// end of the 2nd argument in define method
 
-        // We are creating a foreignkey boject which is the third argument in define method 
-        //to join products model and users model
-        {
-            classMethods: {
-            	associate: function(models) {
-            		Products.belongsTo(models.Users, {
-            			foreignKey: { allowNull: false }		
-            		});// end of belongdsTo method
-            	}//end of associate
-            }// end of classMethod
-        }//fk is 3d argument
-	);// end of define
-	return Products;
-};//exports
+//         // We are creating a foreignkey boject which is the third argument in define method 
+//         //to join products model and users model
+//         {
+//             classMethods: {
+//             	associate: function(models) {
+//             		Products.belongsTo(models.Users, {
+//             			foreignKey: { allowNull: false }		
+//             		});// end of belongdsTo method
+//             	}//end of associate
+//             }// end of classMethod
+//         }//fk is 3d argument
+// 	);// end of define
+// 	return Products;
+// };//exports
--- public/assets/js/login.js
+++ public/assets/js/login.js
@@ -1,2 +1,41 @@
+// grabs url
+var currentURL = window.location.origin;
+
+// hides divs 
+$("#confirmation").hide();
+
+// sends post request with user data and displays confirmation form
+// post sent before confirmation b/c couldn't send post request and show Auth0 widget 
+//--- simultaneously or via other methods so submitLogin sends post and Confirm opens Auth0 widget
+$("#submitLogin").on("click", function(){
+	var userName = $("#userName").val().trim();
+    var userPhone = $("#userPhone").val().trim();
+    if ($(userName !== "")) {
+        var userInfo = {
+            name: userName,
+            phone: userPhone
+        }
+        $("#confName").html(userName);
+        if ($(userPhone !== "")) {
+            $("#confPhone").html(userPhone);
+        } else {
+            $("#confPhone").html("Not Provided");
+        }
+        $("#userName").val("");
+        $("#userPhone").val("");
+        $("#userForm").hide();
+        $("#confirmation").show();
+        $.post("/api/user", userInfo, function(data){});
+    } 
+    return false;
+});
+
+// redisplays user form 
+$("#reenter").on("click", function(){
+    $("#confirmation").hide();
+    $("#userForm").show();
+    return false;
+});
+  
 
   
