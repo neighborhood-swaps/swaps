d00ccdf504ad3cf5beff6bb6dd15bd10dbbaf10b
mwong770 <mwong770@gmail.com>
2017-06-12 09:56:59 -0400

    removes unnecessary comments

--- models/products.js
+++ models/products.js
@@ -1,80 +1,35 @@
-// module.exports = function(sequelize, DataTypes) {
-//     var Products = sequelize.define("Products", { // 2nd argument in define method for columns
-//             product_name: {
-//             	type: DataTypes.STRING,
-//             	allowNull: false,
-//             	validate: { len:[1] }
-//             },// 1st col
-
-//             product_description: {
-//             	type: DataTypes.STRING,
-//             	allowNull: false,
-//             	validate: { len:[1] }
-//             }, //2nd col
-
-//             product_img: {
-//             	type: DataTypes.TEXT,
-//             	allowNull: true,
-//             } //3rd col
-//         },// end of the 2nd argument in define method
-
-//         // We are creating a foreignkey boject which is the third argument in define method
-//         //to join products model and users model
-//         {
-//             classMethods: {
-//             	associate: function(models) {
-//             		Products.belongsTo(models.Users, {
-//             			foreignKey: { allowNull: false }
-//             		});// end of belongdsTo method
-//             	}//end of associate
-//             }// end of classMethod
-//         }//fk is 3d argument
-// 	);// end of define
-// 	return Products;
-// };//exports
-
-//******* by Maria and Adrian *** this is what we got working with what we need **************
 
 module.exports = function(sequelize, DataTypes) {
-    var Products = sequelize.define("Products", { // 2nd argument in define method for columns 
-			// started_at: {
-			//   type: DataTypes.DATE,
-			//   allowNull: false,
-			//   defaultValue: DataTypes.NOW
-			// },
+    var Products = sequelize.define("Products", 
+        { 
             category: {
-            	type: DataTypes.STRING,
-            	// allowNull: false,
-                allowNull: true
-            	// validate: { len:[1] }
-            }, //2nd col
+                type: DataTypes.STRING,
+                allowNull: false,
+                validate: { len:[1] }
+            }, 
             description: {
-            	type: DataTypes.STRING,
-            	// allowNull: false,
-                allowNull: true
-            	// validate: { len:[1] }
-            }, //2nd col
+                type: DataTypes.STRING,
+                allowNull: false,
+                validate: { len:[1] }
+            }, 
             img_location: {
-            	type: DataTypes.STRING,
-            	allowNull: true
+                type: DataTypes.STRING,
+                allowNull: false
             },
-           	condition: {
-            	type: DataTypes.STRING,
-            	// allowNull: false,
-                allowNull: true
-            	// validate: { len:[1] }
+               prod_condition: {
+                type: DataTypes.STRING,
+                allowNull: true,
+                validate: { len:[1] }
             }, 
             availabilitiy: {
-            	type: DataTypes.STRING,
-            	// allowNull: false,
-                allowNull: true
-            	// validate: { len:[1] }
+                type: DataTypes.STRING,
+                allowNull: false,
+                validate: { len:[1] }
             }, 
             swap_location: {
             	type: DataTypes.STRING,
-            	// allowNull: false,
-                allowNull: true
-            	// validate: { len:[1] }
+            	allowNull: false,
+            	validate: { len:[1] }
             },
             comments: {
             	type: DataTypes.STRING,
@@ -82,24 +37,34 @@ module.exports = function(sequelize, DataTypes) {
             }, 
             user_id: {
             	type: DataTypes.STRING,
-            	// allowNull: false,
-                allowNull: true
-            	// validate: { len:[1] }
+            	allowNull: false,
+            	validate: { len:[1] }
             } 
-             //3rd col
-        }// end of the 2nd argument in define method
-
-        // We are creating a foreignkey boject which is the third argument in define method 
-        // to join products model and users model
-        // {
-        //     classMethods: {
-        //     	associate: function(models) {
-        //     		Products.belongsTo(models.Users, {
-        //     			foreignKey: { allowNull: false }		
-        //     		});// end of belongdsTo method
-        //     	}//end of associate
-        //     }// end of classMethod
-        // }//fk is 3d argument
-	);// end of define
+        },
+        {
+            classMethods: {
+            	associate: function(models) {
+            		Products.belongsTo(models.Users, 
+                        {
+                            onDelete: "cascade",
+                            foreignKey: {
+                                // allowNull: false
+                            }
+                        }
+                    );		
+            	}, 
+                associate: function(models) {
+                    Products.belongsTo(models.Locations, 
+                        {
+                            onDelete: "cascade",
+                            foreignKey: {
+                                // allowNull: false
+                            }               
+                        }
+                    );
+                }
+            }
+        }
+	);
 	return Products;
-};//exports
+};
