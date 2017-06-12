bd9c7a8ef9bc79edb36b15a21d5cf6ff44939946
_apj_ <adrian.jensen@gmail.com>
2017-06-12 18:39:00 -0400

    added to seeds file

--- models/products.js
+++ models/products.js
@@ -36,70 +36,70 @@
 //******* by Maria and Adrian *** this is what we got working with what we need **************
 
 module.exports = function(sequelize, DataTypes) {
-    var Products = sequelize.define("Products", { // 2nd argument in define method for columns 
-			// started_at: {
-			//   type: DataTypes.DATE,
-			//   allowNull: false,
-			//   defaultValue: DataTypes.NOW
-			// },
+    var Products = sequelize.define("Products", { // 2nd argument in define method for columns
+            // started_at: {
+            //   type: DataTypes.DATE,
+            //   allowNull: false,
+            //   defaultValue: DataTypes.NOW
+            // },
             category: {
-            	type: DataTypes.STRING,
-            	// allowNull: false,
+                type: DataTypes.STRING,
+                // allowNull: false,
                 allowNull: true
-            	// validate: { len:[1] }
+                    // validate: { len:[1] }
             }, //2nd col
             description: {
-            	type: DataTypes.STRING,
-            	// allowNull: false,
+                type: DataTypes.STRING,
+                // allowNull: false,
                 allowNull: true
-            	// validate: { len:[1] }
+                    // validate: { len:[1] }
             }, //2nd col
             img_location: {
-            	type: DataTypes.STRING,
-            	allowNull: true
+                type: DataTypes.STRING,
+                allowNull: true
             },
-           	condition: {
-            	type: DataTypes.STRING,
-            	// allowNull: false,
+            prod_condition: {
+                type: DataTypes.STRING,
+                // allowNull: false,
                 allowNull: true
-            	// validate: { len:[1] }
-            }, 
+                    // validate: { len:[1] }
+            },
             availabilitiy: {
-            	type: DataTypes.STRING,
-            	// allowNull: false,
+                type: DataTypes.STRING,
+                // allowNull: false,
                 allowNull: true
-            	// validate: { len:[1] }
-            }, 
+                    // validate: { len:[1] }
+            },
             swap_location: {
-            	type: DataTypes.STRING,
-            	// allowNull: false,
+                type: DataTypes.STRING,
+                // allowNull: false,
                 allowNull: true
-            	// validate: { len:[1] }
+                    // validate: { len:[1] }
             },
             comments: {
-            	type: DataTypes.STRING,
-            	allowNull: true,
-            }, 
+                type: DataTypes.STRING,
+                allowNull: true,
+            },
             user_id: {
-            	type: DataTypes.STRING,
-            	// allowNull: false,
+                type: DataTypes.STRING,
+                // allowNull: false,
                 allowNull: true
-            	// validate: { len:[1] }
-            } 
-             //3rd col
-        }// end of the 2nd argument in define method
+                    // validate: { len:[1] }
+            }
+            //3rd col
+        } // end of the 2nd argument in define method
 
-        // We are creating a foreignkey boject which is the third argument in define method 
+        // We are creating a foreignkey boject which is the third argument in define method
         // to join products model and users model
         // {
         //     classMethods: {
         //     	associate: function(models) {
         //     		Products.belongsTo(models.Users, {
-        //     			foreignKey: { allowNull: false }		
+        //     			foreignKey: { allowNull: false }
         //     		});// end of belongdsTo method
         //     	}//end of associate
         //     }// end of classMethod
         // }//fk is 3d argument
-	);// end of define
-	return Products;
-};//exports
+    ); // end of define
+    return Products;
+}; //exports
