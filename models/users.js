5d4926e344f3bfa97b1498efb250062bc1d25c96
mwong770 <mwong770@gmail.com>
2017-06-11 20:21:08 -0400

    fixes spacing

--- models/users.js
+++ models/users.js
@@ -112,50 +112,49 @@
 //                         }; // end of module.exports
 
 
-// **
+
 // ****************** WORKING DATABASE ********************
 
 module.exports = function(sequelize, DataTypes) {
     var Users = sequelize.define("Users", 
-            {
-                user_id: {
-                    type: DataTypes.STRING,
-                    primaryKey: true,
-                    autoIncrement: false,
-                    allowNull: false,
-                    validate: { len: [1] } 
-                },
-                first_name: {
-                    type: DataTypes.STRING,
-                    allowNull: false,
-                    validate: { len: [1] }
-                }, 
-                last_name: {
-                    type: DataTypes.STRING,
-                    allowNull: false,
-                    validate: { len: [1] }
-                }, 
-                user_email: {
-                    type: DataTypes.STRING,
-                    allowNull: false,
-                    validate: { isEmail: true }
-                }
-            }, 
-            { 
-                timestamps: false
+        {
+            user_id: {
+                type: DataTypes.STRING,
+                primaryKey: true,
+                autoIncrement: false,
+                allowNull: false,
+                validate: { len: [1] } 
             },
-            {
-                classMethods: {
-                    associate: function(models) {
-                        Users.hasMany(models.Products, 
-                            { 
-                                onDelete: "cascade",
-                            }
-                        ); 
-                    } 
-                }
-            } 
-        
+            first_name: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                validate: { len: [1] }
+            }, 
+            last_name: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                validate: { len: [1] }
+            }, 
+            user_email: {
+                type: DataTypes.STRING,
+                allowNull: false,
+                validate: { isEmail: true }
+            }
+        }, 
+        { 
+            timestamps: false
+        },
+        {
+            classMethods: {
+                associate: function(models) {
+                    Users.hasMany(models.Products, 
+                        { 
+                            onDelete: "cascade",
+                        }
+                    ); 
+                } 
+            }
+        } 
     ); 
     return Users;
 };
