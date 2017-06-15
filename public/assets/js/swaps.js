7322773c38d4a27573bd0fc3b962cbca3ecaf604
mwong770 <mwong770@gmail.com>
2017-06-15 01:57:37 -0400

    adds working contact form that sends email

--- public/assets/js/swaps.js
+++ public/assets/js/swaps.js
@@ -14,3 +14,8 @@ $(document).on("click", "#swapbtn", function() {
     startSwap();
 
 });
+
+$(document).ready(function(){
+    // the "href" attribute of .modal-trigger must specify the modal ID that wants to be triggered
+    $('.modal-trigger').leanModal();
+  });
